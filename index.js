b44c1268bc5971a76cc63158b3894eadc606efa0
shashidar mandalozu <sasitech18@gmail.com>
2020-10-10 20:38:27 +0100

    Adding layouts using if else statements

--- index.js
+++ index.js
@@ -51,7 +51,21 @@ function createBoard() {
         // put square in squares array
         squares.push(square);
         //console.log(squares);
+
+        // Adding layouts using if else statements
+        if(layout[i] === 0) {
+            squares[i].classList.add('pac-dot');
+        } else if(layout[i] === 1) {
+            squares[i].classList.add('wall')
+        } else if (layout[i] === 2) {
+            squares[i].classList.add('ghost-liar') 
+        } else if (layout[i] === 3) {
+            squares[i].classList.add('power-pellet')
+        } else {
+            squares[i].classList.add('empty')
+        }
     }
+    //console.log(squares);
 }
 createBoard();
 
